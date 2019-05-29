dcb03b5e7893011813d06bb3ce5eeb3bbb33db5e
lc <1259493451@qq.com>
2019-05-29 20:28:04 +0800

    s

---
+++ hs/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    /*1.引入*/
+    /*2.结构   HTML中需要视差滚动效果的加  data-stellar-background-ratio="0.5"*/
+    /*3.初始化插件*/
+    $.stellar({
+        horizontalScrolling: false,
+        responsive: true
+    });
+});
