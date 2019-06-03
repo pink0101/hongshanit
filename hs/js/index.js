0d1f2f65d20dec43a85cda737c4cecb30e3b197e
lc <1259493451@qq.com>
2019-06-03 18:49:54 +0800

    更新轮播图模块

--- hs/js/index.js
+++ hs/js/index.js
@@ -11,6 +11,7 @@ $(function () {
 
 /*导航栏点击事件*/
 $(function () {
+    /**/
     $('.hs_nav_switch').on('click', function () {
         $('.hs_nav_left').fadeIn();
     });
@@ -19,11 +20,70 @@ $(function () {
     });
 
 
-    window.onresize = function () {
+    window.onresize = function () {//对屏幕大小发生改变的时候执行
         var width = $(window).width();//获取屏幕宽度
         if (width > 992) {
             $('.hs_nav_left').fadeIn();
+        } else if (width < 992) {
+            $('.hs_nav_left').css('display', 'none')
         }
     };
+});
 
+/*轮播图部分*/
+$(function () {
+    //假设一个li的索引值
+    var count = 0;
+    //拿到右边的按钮，注册点击事件
+    $(".arrow-right").click(function () {
+        //判断索引是否到达最大值，如果有，重新赋值为1，如果没有自增
+        count == $(".slider .slider_div li").length - 1 ? count = 0 : count++;
+        //将索引值赋给li，然后设置显示，然后找到所有兄弟元素，设置消失
+        $(".slider .slider_div li").eq(count).css('display', 'block').siblings("li").css('display', 'none');
+
+        var width=$(document).width();
+        if (width<992){
+            $(".slider .slider_spot li p").css('background', '#ffffff');
+            //将所有的p的背景颜色的清除，然后将当前的p加上背景颜色
+            $(".slider .slider_spot li p").eq(count).css('background', '#9f8027');
+        } else{
+            $(".slider .slider_spot li p").css('background', 'none');
+            //将所有的p的背景颜色的清除，然后将当前的p加上背景颜色
+            $(".slider .slider_spot li p").eq(count).css('background', '#9f8027');
+        }
+
+    });
+
+    setInterval(function () {
+        //判断索引是否到达最大值，如果有，重新赋值为1，如果没有自增
+        count == $(".slider .slider_div li").length - 1 ? count = 0 : count++;
+        //将索引值赋给li，然后设置显示，然后找到所有兄弟元素，设置消失
+        $(".slider .slider_div li").eq(count).css('display', 'block').siblings("li").css('display', 'none');
+
+        var width=$(document).width();
+        if (width<992){
+            $(".slider .slider_spot li p").css('background', '#ffffff');
+            //将所有的p的背景颜色的清除，然后将当前的p加上背景颜色
+            $(".slider .slider_spot li p").eq(count).css('background', '#9f8027');
+        } else{
+            $(".slider .slider_spot li p").css('background', 'none');
+            //将所有的p的背景颜色的清除，然后将当前的p加上背景颜色
+            $(".slider .slider_spot li p").eq(count).css('background', '#9f8027');
+        }
+    }, 5000);
+
+
+
+
+    //拿到左边的按钮，注册点击事件
+    $(".arrow-left").click(function () {
+        //判断索引是否到达最小值，如果有，重新赋值为最大值，如果没有自减
+        count == 0 ? count = $(".slider .slider_div li").length - 1 : count--;
+        //将索引值赋给li，然后设置显示，然后找到所有兄弟元素，设置消失
+        $(".slider .slider_div li").eq(count).css('display', 'block').siblings("li").css('display', 'none');
+
+        //将所有的p的背景颜色的清除，然后将当前的p加上背景颜色
+        $(".slider .slider_spot li p").css('background', 'none');
+        $(".slider .slider_spot li p").eq(count).css('background', '#9f8027');
+    })
 });
